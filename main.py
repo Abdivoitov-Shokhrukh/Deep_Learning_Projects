2ecaebb0fc159dcd78ba682981d4f0e48a95c6aa
Abdivoitov Shokhrukh <120116161+Abdivoitov-Shokhrukh@users.noreply.github.com>
2024-03-25 21:53:32 +0500

    project files have been uploaded

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+#title
+st.title("This is project which indentify Sport Equipments like Parachute, training bench, Snowboard, Ski, Lifejacket, Punching bag, and Surfboard")
+
+
+#Image uploading
+file = st.file_uploader("Rasm yuklash", type = ['png','jpeg','gif','svg','jpg','webp'])
+
+if file:
+    st.image(file)
+    #PIL convert
+    img = PILImage.create(file)
+
+    #model
+    model = load_learner("Sport_equipment_classifier.pkl")
+
+    #Prediction
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat:{pred}")
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+
+    #Plotting
+    fig = px.bar(x=probs*100, y = model.dls.vocab)
+    st.plotly_chart(fig)
